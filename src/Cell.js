fcb0c75a268c6155483f5c4c24338e30cffb892a
Henry Jiang <henryjiang97@gmail.com>
2020-11-10 14:33:20 -0800

    Styled Cell.js

--- src/Cell.js
+++ src/Cell.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-
 import './Cell.css';
 
 let CELL_SIZE = 1;
@@ -14,24 +13,25 @@ class Cell extends Component {
     }
 
     // Set cell size based on 
-    setCellSize(){
+    setCellSize() {
         const x = this.props.x;
         const y = this.props.y;
         let xSize = 450 / x;
         let ySize = 1200 / y;
         let cell_size = Math.min(xSize, ySize);
         CELL_SIZE = Math.max(cell_size, 1);
-        if(CELL_SIZE <= 10){
+        if (CELL_SIZE <= 10) {
             BORDER_WIDTH = 1
         }
-        if(CELL_SIZE === 1){
+        if (CELL_SIZE === 1) {
             BORDER_WIDTH = 0.5
         }
     }
 
     // Change alive state when clicking on cell
     handleClick() {
-        let i = this.props.i, j = this.props.j;
+        let i = this.props.i;
+        let j = this.props.j;
         this.props.flipCell(i, j);
     }
 
@@ -43,15 +43,21 @@ class Cell extends Component {
 
         let gradient = Math.max(0, 1 - diff / (this.props.frequency / 100));
 
-        if(this.props.alive)    return 'rgba(0,0,0)';
-        if(gradient === 0)    return 'rgba(255,255,255)';
+        if (this.props.alive) return 'rgba(0,0,0)';
+        if (gradient === 0) return 'rgba(255,255,255)';
         else return 'rgba(0,0,0,' + gradient.toString() + ')';
     }
 
     render () {
         return (
-            //<div className='Cell' style={{backgroundColor: `rgba(0, 0, 0, ${this.getGradient()})`}} onClick={this.handleClick}/>
-            <div className='Cell' style={{ width: CELL_SIZE, height: CELL_SIZE, borderWidth: BORDER_WIDTH, backgroundColor: this.getGradient()}} onClick={this.handleClick}/>
+            <div className='Cell' 
+                 style={{ 
+                     width: CELL_SIZE, 
+                     height: CELL_SIZE, 
+                     borderWidth: BORDER_WIDTH, 
+                     backgroundColor: this.getGradient()
+                 }}
+                 onClick={this.handleClick}/>
         );
     }
 }
