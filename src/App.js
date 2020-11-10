c30d48748c5f2c3fa825fd27423d80439282b2ed
Henry Jiang <henryjiang97@gmail.com>
2020-11-10 14:29:40 -0800

    Rebuilt

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch } from "react-router-dom";
 
 import './App.css';
 import Home from './Home';
-import Infos from './Infos';
+import Infos from "./Infos";
 
 export default function App() {
-    return (
-        <div className="App">
-            <nav className="navbar navbar">
-                <ul className="nav navbar-nav">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/infos">Infos</Link>
-                    </li>
-                </ul>
-            </nav>
+  return (
+    <div className = "App">
+      <nav className="navbar navbar">
+        <ul className="nav navbar-nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/infos">Infos</Link>
+          </li>
+        </ul>
+      </nav>
 
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/infos" component={Infos} />
-            </Switch>
-        </div>
-    );
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/infos" component={Infos} />
+      </Switch>
+    </div>
+  );
 }
+
+
